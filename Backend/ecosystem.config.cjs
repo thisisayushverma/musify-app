ed873cc3dd9dfe4638022821500e62dd2f435fca
Ayush Verma <devilofilluminati@gmail.com>
2025-07-26 22:12:56 +0530

    changes on ecosystem.cje

--- Backend/ecosystem.config.cjs
+++ Backend/ecosystem.config.cjs
@@ -7,5 +7,23 @@ module.exports =  {
     env: {
         NODE_ENV: 'production'
       }
+  },
+  {
+    name: "S3UploaderWorker",
+    script: "./worker/s3Uploader.worker.js",
+    autorestart: true,
+    watch: false,
+  },
+  {
+    name: "abrWorker",
+    script: "./worker/abr.worker.js",
+    autorestart: true,
+    watch: false,
+  },
+  {
+    name: "wavWorker",
+    script: "./worker/wav.worker.js",
+    autorestart: true,
+    watch: false,
   }]
 }
