aff830c84b98bf892ace0162f8356b888e726038
Ayush Verma <devilofilluminati@gmail.com>
2025-07-26 23:17:35 +0530

    updating same site to nono

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -7,11 +7,13 @@ import { generateOtp } from "./otp.controller.js";
 
 const accessTokenCookieOption = {
     httpOnly: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
 }
 
 const refreshTokenCookieOption = {
     httpOnly: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000
 }
 
